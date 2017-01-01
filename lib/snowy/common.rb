c05a9416e835897e06fe5608a0738f48f75e45e2
dearblue <dearblue@users.noreply.github.com>
2017-01-01 18:24:35 +0900

    複数の隣接する三角形を合成して多角形にするメソッドの追加

--- lib/snowy/common.rb
+++ lib/snowy/common.rb
@@ -54,6 +54,96 @@ module Snowy
            (a.to_i.clamp(0, 255)      )
   end
 
+  module Aux
+    def self.build_shape_from_triangle(triangles)
+      shapes = [triangles.map { |t| t.each_slice(2).to_a }]
+      m = Snowy::Matrix.new([[0.5, -1.5, 0], [0.5, 0.5, 0], [0, 0, 1]])
+      5.times do
+        shapes << shapes[-1].map { |sh|
+          sh.map { |v|
+            (x, y) = m.transform(*v)
+            [x.round, y.round]
+          }
+        }
+      end
+      shapes.flatten!(1)
+      Aux.compose_shapes(shapes)
+    end
+
+    # 同じ辺を持つ多角形を合成する。
+    # ただし、多角形の辺の向きは同一である必要がある。
+    # (右回りの図形であればそれのみ)
+    def self.compose_shapes(shapes)
+      i = 0
+      while i < shapes.size
+        p = shapes[i]
+        j = i + 1
+        while j < shapes.size
+          q = shapes[j]
+          if ss = compose_shape(p, q)
+            shapes.delete_at(j)
+            shapes.concat ss
+            j = i + 1 # j を最初から繰り返す
+          else
+            j += 1
+          end
+        end
+        i += 1
+      end
+
+      shapes.delete_if(&:empty?)
+
+      shapes
+    end
+
+    # 穴が空いた場合、新しく出来たその多角形を返す。
+    # 穴開きの図形は引数で与えた図形とは逆回りのものとなる。
+    def self.compose_shape(p, q)
+      p.each_with_index do |p2, i|
+        p1 = p[i - 1]
+        q.each_with_index do |q2, j|
+          q1 = q[j - 1]
+          if p1 == q2 && p2 == q1
+            # p と q を合成する
+            sub = q.slice(0, j - 1)
+            p[i, 0] = sub if sub
+            sub = q.slice(j + 1, q.size - (sub || []).size - 2)
+            p[i, 0] = sub if sub
+            return cleanup_shape(p, i, i + q.size - 1)
+          end
+        end
+      end
+
+      nil
+    end
+
+    def self.cleanup_shape(p, j0, jj)
+      holes = []
+      i = jj
+      # i と j の値は増減するため、each などのイテレータは使えない
+      while i < p.size
+        (p1, p2) = p.values_at(i - 1, i)
+        j = j0
+        while j < jj
+          (q1, q2) = p.values_at(j - 1, j)
+          if p1 == q2 && p2 == q1
+            hole = p.slice!(j .. i)
+            if hole.size >= 5
+              hole[-2, 2] = []
+              holes << hole
+            end
+            i = j
+            jj = j - 1
+          else
+            j += 1
+          end
+        end
+        i += 1
+      end
+      holes
+    end
+  end
+
 
   #
   # call-seq:
