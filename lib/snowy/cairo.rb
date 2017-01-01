030d7be411f86e1c9ffcc97c1f4ce74db473c6ba
dearblue <dearblue@users.noreply.github.com>
2017-01-01 18:31:50 +0900

    Snowy::CairoDriver による棘が削除されるように修正

--- lib/snowy/cairo.rb
+++ lib/snowy/cairo.rb
@@ -6,30 +6,30 @@ module Snowy
 
   module CairoDriver
     def self.render(size, triangles, background, color, outline, angle)
+      shapes = Aux.build_shape_from_triangle(triangles)
       deg2rad = Math::PI / 180
+      sqrt3 = Math.sqrt(3)
       surface = Cairo::ImageSurface.new(Cairo::Format::ARGB32, size, size)
       Cairo::Context.new(surface) do |context|
         context.instance_eval do
-          set_line_width 0.5
           set_source_color [background.get_red / 255.0, background.get_green / 255.0, background.get_blue / 255.0, background.get_alpha / 255.0]
           paint
           translate(size / 2.0, size / 2.0)
           scale(size / 32.0, size / 32.0)
-          rotate(angle * deg2rad) unless deg2rad == 0
-          sqrt3 = Math.sqrt(3)
-          [30, 90, 150, 210, 270, 330].each do |deg|
-            save do
-              rotate(-deg * deg2rad)
-              scale(1, sqrt3)
-              triangles.each do |(x1, y1, x2, y2, x3, y3)|
-                move_to(x1, y1)
-                line_to(x2, y2)
-                line_to(x3, y3)
-                close_path
+          save do
+            rotate(angle * deg2rad) unless angle == 0
+            rotate(-30 * deg2rad)
+            scale(1, sqrt3)
+            shapes.each do |sh|
+              move_to(*sh[0])
+              sh.slice(1 .. -1).each do |(x, y)|
+                line_to(x, y)
               end
+              close_path
             end
           end
           if outline
+            set_line_width 0.5
             set_source_rgba outline.get_red / 255.0, outline.get_green / 255.0, outline.get_blue / 255.0, 255 / 255.0
             stroke true
           end
