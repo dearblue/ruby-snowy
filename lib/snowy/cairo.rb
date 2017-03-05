5750dcff491a20590de39628290952f4a7593b56
dearblue <dearblue@users.noreply.github.com>
2017-03-05 23:16:51 +0900

    CairoDriver.render にて三角形を纏めることなく棘をなくして高速化

--- lib/snowy/cairo.rb
+++ lib/snowy/cairo.rb
@@ -6,26 +6,27 @@ module Snowy
 
   module CairoDriver
     def self.render(size, triangles, background, color, outline, angle)
-      shapes = Aux.build_shape_from_triangle(triangles)
       deg2rad = Math::PI / 180
       sqrt3 = Math.sqrt(3)
       surface = Cairo::ImageSurface.new(Cairo::Format::ARGB32, size, size)
       Cairo::Context.new(surface) do |context|
         context.instance_eval do
+          set_fill_rule Cairo::FillRule::EVEN_ODD
+          set_line_join Cairo::LineJoin::ROUND
           set_source_color [background.red, background.green, background.blue, background.alpha]
           paint
           translate(size / 2.0, size / 2.0)
           scale(size / 32.0, size / 32.0)
           save do
             rotate(angle * deg2rad) unless angle == 0
-            rotate(-30 * deg2rad)
-            scale(1, sqrt3)
-            shapes.each do |sh|
-              move_to(*sh[0])
-              sh.slice(1 .. -1).each do |(x, y)|
-                line_to(x, y)
+            [30, 90, 150, 210, 270, 330].each do |deg|
+              save do
+                rotate deg * deg2rad
+                scale 1, sqrt3
+                triangles.each do |t|
+                  triangle *t
+                end
               end
-              close_path
             end
           end
           if outline
